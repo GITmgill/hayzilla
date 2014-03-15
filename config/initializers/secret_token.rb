7d722142b7093cf574aa7963015832f7c49b66e9
mGill <mark@bpisports.net>
2014-03-15 11:05:59 -0400

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hayzilla::Application.config.secret_key_base = '6d6b91f2ad12c9543d55bf732802e6e0767e230bc07e9ee21ea5e0db6230bc72f9154b2270ee7c7e90eeda7da5dfe220524afa26b244a78069178235f7dc374f'
